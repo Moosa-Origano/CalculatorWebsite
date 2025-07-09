53530ac9ff0680c34e587d2f395bf438ecfc6cc0
Moosa-Origano <musa9islam@gmail.com>
2025-07-09 15:19:11 +0100

    Added JS and Design Tweaks
    Changed the layout of the output screen.
    Implemented the JavaScript so that calculations now show up when being entered. All buttons now at least add to the current operations screen, Eval doesn't work for divide, multiple or minus, but does for plus and mod. Also need to modify brackets so numbers can be put into them.

---
+++ scripts/homePage.js
@@ -0,0 +1,56 @@
+let currentCalc = document.getElementById("current-calc")
+let output = document.getElementById("output")
+
+const clear = document.getElementById("C")
+const brackets = document.getElementById("brackets")
+const mod = document.getElementById("mod")
+const divide = document.getElementById("divide")
+
+const seven = document.getElementById("7")
+const eight = document.getElementById("8")
+const nine = document.getElementById("9")
+const times = document.getElementById("times")
+
+const four = document.getElementById("4")
+const five= document.getElementById("5")
+const six = document.getElementById("6")
+const minus= document.getElementById("minus")
+
+
+const one = document.getElementById("1")
+const two = document.getElementById("2")
+const three = document.getElementById("3")
+const plus = document.getElementById("plus")
+
+
+const del = document.getElementById("DEL")
+const zero = document.getElementById("0")
+const decimal = document.getElementById(".")
+const equals = document.getElementById("equals")
+
+
+const clearAll = () => {
+  output.innerHTML = ""; 
+  currentCalc.innerHTML = ""
+} // function that clears the screen
+
+const deleteLast = () => {
+  if (currentCalc.innerHTML.length > 0) {
+    currentCalc.innerHTML = currentCalc.innerHTML.substring(0, currentCalc.innerHTML.length - 1);
+  }
+} 
+
+
+clear.onclick = () => {output.innerHTML = ""; currentCalc.innerHTML = ""}
+brackets.onclick = () => {currentCalc.innerHTML += "()"}
+mod.onclick = () => {}
+
+
+
+del.onclick = () => {
+  if (currentCalc.innerHTML.length > 0) {
+    currentCalc.innerHTML = currentCalc.innerHTML.substring(0, currentCalc.innerHTML.length - 1);
+  }
+}
+
+
